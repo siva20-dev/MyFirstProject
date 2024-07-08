ec15f8dcf0297a04f7537cca3a0fe2d3a743b3d9
siva20-dev <psivark07@gmail.com>
2024-07-08 11:41:02 +0530

    Class Created

---
+++ Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+        public int id { get; set; }
+        public string name { get; set; }
+        public int age { get; set; }
+    }
+}
